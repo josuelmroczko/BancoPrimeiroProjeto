d11171ceb2fe60bc035e2a916a97b8e35571c10d
josuel <105312703+josuelmroczko@users.noreply.github.com>
2022-08-16 15:34:27 -0300

    Initial commit

---
+++ app/src/main/java/com/banco/bancobanco/Senha.java
@@ -0,0 +1,54 @@
+
+package com.banco.bancobanco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Senha extends AppCompatActivity {
+
+    private EditText senha ;
+    private Button botaoConfirmar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senha);
+
+        senha= findViewById(R.id.edit_senha);
+        botaoConfirmar= findViewById(R.id.bt_confirmar);
+
+getSupportActionBar().hide();
+    botaoConfirmar.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int senhaCadastrada = 12345;
+            int senhaAtual;
+
+            senhaAtual = Integer.parseInt(senha.getText().toString());
+
+            if (senhaAtual == senhaCadastrada){
+                irParaActivityPrincipal();
+            }else {
+                Toast.makeText(Senha.this,"senha esta incorreta",Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+    });
+
+    }
+    private void irParaActivityPrincipal(){
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
